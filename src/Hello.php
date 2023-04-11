4a54cd9d2640b4f5925c7a773619602f67c239d4
johann gonin <[MY\_johann.g.37@gmail.com](mailto:johann.g.37@gmail.com)>
2023-04-11 07:17:11 +0200

    first commit

---
+++ src/Hello.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class Hello
+{
+
+    public function hello(): string
+    {
+        return 'Hello World!';
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jg223/composer2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '09c93d3c199fd7b1db4add205875c5be9cfc4e4a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jg223/composer2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '09c93d3c199fd7b1db4add205875c5be9cfc4e4a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
